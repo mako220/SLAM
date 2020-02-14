74b0458b346c566a221fca989b925533e8fd705f
mako220 <kshsnh220@gmail.com>
2020-02-14 17:48:53 +0530

    Fixed tracking for video

--- prog.py
+++ prog.py
@@ -2,38 +2,36 @@
 
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt 
-#from matplotlib import pyplot as plt
-from IPython import get_ipython
-ipy = get_ipython()
-if ipy is not None:
-    ipy.run_line_magic('matplotlib', 'inline')
+
 
 cap = cv2.VideoCapture('test1.mp4')
 
+#def nothing(x):
+#    pass
+
+cv2.namedWindow("Frame")
+#cv2.createTrackbar("quality","Frame", 1, 100,nothing)
+
 while(cap.isOpened()):
-    ret, frame = cap.read()
+    _, frame = cap.read()
 
     img = cv2.resize(frame , (1920//2,1080//2))
 
-#    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = np.mean(img, axis=2).astype(np.uint8)
     #print(img.shape)
     #print(img)
     corners = cv2.goodFeaturesToTrack(gray,maxCorners=1000,qualityLevel=0.01,minDistance=3)
-#    corners = corners.astype(np.int)[:,0,:]
     corners = np.int0(corners)
+
     print(corners)
     for i in corners:
          x,y = i.ravel()
-         cv2.circle(img,(x,y),3,(255,0,0),0)
-    #cv2.imshow('video',img)
-    #cv2.show()
-    plt.imshow(img),plt.show()
-    cv2.waitKey(25)
-    continue
-    if cv2.waitKey(0) & 0xFF == ord('q'):
-            break
+         cv2.circle(img,(x,y),3,(255,0,0),-1)
+   
+    cv2.imshow("Frame", img)
+    key = cv2.waitKey(1)
+    if key == 27:
+         break
 
 cap.release()
 cv2.destroyAllWindows()
